282416c0882e6ea71918bc1eb8a2e7c0b9732f81
Ivan Peťko <ipetko96@gmail.com>
2023-11-13 15:43:34 +0100

    write output to file

--- logo.py
+++ logo.py
@@ -15,41 +15,44 @@ allowed = {
 
 
 space = 0
-while len(file) > 0:
-    instruction = file.split(" ")[0]
-    if instruction == '[]':
-        print(f"{' ' * space}pass")
-    if instruction == '[':
+with open('logo-out.py', 'w',) as out:
+    out.write('import turtle\nt = turtle.Turtle()\n')
+    while len(file) > 0:
+        instruction = file.split(" ")[0]
+        if instruction == '[]':
+            out.write(f"{' ' * space}pass\n")
+        if instruction == '[':
+            file = file[len(instruction) :].lstrip()
+            continue
+        if instruction[0] == '[':
+            file = file[1:].lstrip()
+            continue
+        if instruction[0] == ']':
+            space -= 4
+            file = file[1:].lstrip()
+            continue
+        if instruction.split(']')[0] not in allowed:
+            instruction = instruction.split(']')[0]
+            out.write(f"{' ' * space}{instruction}()\n")
+            file = file[len(instruction) :].lstrip()
+            continue
         file = file[len(instruction) :].lstrip()
-        continue
-    if instruction[0] == '[':
-        file = file[1:].lstrip()
-        continue
-    if instruction[0] == ']':
-        space -= 4
-        file = file[1:].lstrip()
-        continue
-    if instruction.split(']')[0] not in allowed:
-        instruction = instruction.split(']')[0]
-        print(f"{' ' * space}{instruction}()")
-        file = file[len(instruction) :].lstrip()
-        continue
-    file = file[len(instruction) :].lstrip()
-    next_instruction = file.split(" ")[0]
-    if '[' in next_instruction:
-        next_instruction = next_instruction.partition("[")[0]
-    elif instruction == 'repeat':
-        print(f"{' ' * space}{allowed[instruction.replace('[','')]}(1, {int(next_instruction)+1}):")
-        space += 4
-    elif next_instruction.isnumeric() or "'" in next_instruction or '+' in next_instruction:
-        print(f"{' ' * space}t.{allowed[instruction.replace('[','')]}({next_instruction})")
-    elif instruction == "to":
-        space = 4
-        print(f"{allowed[instruction]} {next_instruction}():")
-    elif ']' in next_instruction:
-        print(f"{' ' * space}t.{allowed[instruction.replace('[','')]}({next_instruction.replace(']','')})")
-        space = 0
-    else:
-        print(f"{' ' * space}t.{allowed[instruction]}()")
-        continue
-    file = file[len(next_instruction) :].lstrip()
+        next_instruction = file.split(" ")[0]
+        if '[' in next_instruction:
+            next_instruction = next_instruction.partition("[")[0]
+        if instruction == 'repeat':
+            out.write(f"{' ' * space}{allowed[instruction.replace('[','')]}(1, {int(next_instruction)+1}):\n")
+            space += 4
+        elif next_instruction.isnumeric() or "'" in next_instruction or '+' in next_instruction:
+            out.write(f"{' ' * space}t.{allowed[instruction.replace('[','')]}({next_instruction})\n")
+        elif instruction == "to":
+            space = 4
+            out.write(f"{allowed[instruction]} {next_instruction}():\n")
+        elif ']' in next_instruction:
+            out.write(f"{' ' * space}t.{allowed[instruction.replace('[','')]}({next_instruction.replace(']','')})\n")
+            space = 0
+        else:
+            out.write(f"{' ' * space}t.{allowed[instruction]}()\n")
+            continue
+        file = file[len(next_instruction) :].lstrip()
+    out.write('turtle.done()\n')
