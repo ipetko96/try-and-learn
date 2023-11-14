77a9aeb95780653b2524f1be4a7e23c32a3e8944
Ivan Peťko <ipetko96@gmail.com>
2023-11-14 20:27:50 +0100

    logo v5

--- logo.py
+++ logo.py
@@ -1,67 +1,86 @@
 with open("logo-vstup") as file:
-    file = file.read().replace("\n", " ").lstrip()
+    file = (
+        file.read().replace("\n", " ").replace("[", " [ ").replace("]", " ] ").lstrip()
+    )
+# file = 'lt 90 pu fd 100 rt 30 pd to krok  [ fd 30 rt 120 ]   repeat 4  [    fd 50   lt 10+20   repeat 3 [ krok ] lt 60  ]  rt 30 fd 70 repeat 10 [  ] '
 
-allowed_key=["fd",
-"rt",
-"lt",
-"pu",
-"pd",
-"setpc",
-"setpw",
-"repeat",
-"to"]
+# t = turtle.Turtle()
+# t.lt(90)
+# t.pu()
+# t.fd(100)
+# t.rt(30)
+# t.pd()
+# def krok():
+#     t.fd(30)
+#     t.rt(120)
+# for repc in range(1, 5):
+#     t.fd(50)
+#     t.lt(10+20)
+#     for repc in range(1, 4):
+#         krok()
+#     t.lt(60)
+# t.rt(30)
+# t.fd(70)
+# for repc in range(1, 11):
+#     pass
+
+allowed_key = ["fd", "rt", "lt", "pu", "pd", "setpc", "setpw", "repeat", "to"]
 allowed_value = [
-     "fd",
-     "rt",
-     "lt",
-     "pu",
-     "pd",
-     "pencolor",
-     "pensize",
-     "for repc in range",
-     "def"
+    "t.fd(",
+    "t.rt(",
+    "t.lt(",
+    "t.pu()\n",
+    "t.pd()\n",
+    "t.pencolor(",
+    "pensize",
+    "for repc in range(1, ",
+    "def ",
 ]
 
 
-space = 0
-with open('logo-out.py', 'w',) as out:
-    out.write('import turtle\nt = turtle.Turtle()\n')
+with open(
+    "logo-out.py",
+    "w",
+) as out:
+    out.write("import turtle\nt = turtle.Turtle()\n")
+    instruction = False
+    space = 0
+    func = False
+    repeat = False
+    color = False
     while len(file) > 0:
-        instruction = file.split(" ")[0]
-        if instruction == '[]':
-            out.write(f"{' ' * space}pass\n")
-        if instruction == '[':
-            file = file[len(instruction) :].lstrip()
-            continue
-        if instruction[0] == '[':
-            file = file[1:].lstrip()
-            continue
-        if instruction[0] == ']':
-            space -= 4
-            file = file[1:].lstrip()
-            continue
-        if instruction.split(']')[0] not in allowed_key:
-            instruction = instruction.split(']')[0]
-            out.write(f"{' ' * space}{instruction}()\n")
-            file = file[len(instruction) :].lstrip()
-            continue
-        file = file[len(instruction) :].lstrip()
-        next_instruction = file.split(" ")[0]
-        if '[' in next_instruction:
-            next_instruction = next_instruction.partition("[")[0]
-        if instruction == 'repeat':
-            out.write(f"{' ' * space}{allowed_value[allowed_key.index(instruction.replace('[',''))]}(1, {int(next_instruction)+1}):\n")
-            space += 4
-        elif next_instruction.isnumeric() or "'" in next_instruction or '+' in next_instruction:
-            out.write(f"{' ' * space}t.{allowed_value[allowed_key.index(instruction.replace('[',''))]}({next_instruction})\n")
-        elif instruction == "to":
-            space = 4
-            out.write(f"{allowed_value[allowed_key.index(instruction)]} {next_instruction}():\n")
-        elif ']' in next_instruction:
-            out.write(f"{' ' * space}t.{allowed_value[allowed_key.index(instruction.replace('[',''))]}({next_instruction.replace(']','')})\n")
-            space = 0
-        else:
-            out.write(f"{' ' * space}t.{allowed_value[allowed_key.index(instruction)]}()\n")
-            continue
-        file = file[len(next_instruction) :].lstrip()
-    out.write('turtle.done()\n')
+        last = file.split(" ")[0]
+        if not func and not repeat and not color:
+            if last in allowed_key:
+                instruction = True
+                if last == "to":
+                    func = True
+                elif last == "setpc":
+                    color = True
+                elif last == "repeat":
+                    repeat = True
+                    instruction = False
+                out.write(f"{' ' * space}{allowed_value[allowed_key.index(last)]}")
+            elif last == "[":
+                space += 4
+                if file[1:].lstrip().split(" ")[0] == "]":
+                    out.write(f"{' ' * space}pass\n")
+            elif last == "]":
+                space -= 4
+            elif not instruction:
+                out.write(f"{' ' * space}{last}()\n")
+            elif str(abs(eval(last))).isnumeric():
+                out.write(f"{last})\n")
+                instruction = False
+        elif repeat:
+            out.write(f"{int(last)+1}):\n")
+            repeat = False
+        elif func:
+            out.write(f"{last}():\n")
+            func = False
+        elif color:
+            last = file[: len(file[1:].split("'")[0]) + 2]
+            out.write(f"{last})\n")
+            color = False
+        file = file[len(last) :].lstrip()
+    out.write("turtle.done()\n")
