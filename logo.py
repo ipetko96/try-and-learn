fde429d30d3e89cf407313a826c92670674ffb73
Ivan Peťko <ipetko96@gmail.com>
2023-11-14 09:54:43 +0100

    replace dict with lists

--- logo.py
+++ logo.py
@@ -1,17 +1,26 @@
 with open("logo-vstup") as file:
     file = file.read().replace("\n", " ").lstrip()
 
-allowed = {
-    "fd": "fd",
-    "rt": "rt",
-    "lt": "lt",
-    "pu": "pu",
-    "pd": "pd",
-    "setpc": "pencolor",
-    "setpw": "pensize",
-    "repeat": "for repc in range",
-    "to": "def",
-}
+allowed_key=["fd",
+"rt",
+"lt",
+"pu",
+"pd",
+"setpc",
+"setpw",
+"repeat",
+"to"]
+allowed_value = [
+     "fd",
+     "rt",
+     "lt",
+     "pu",
+     "pd",
+     "pencolor",
+     "pensize",
+     "for repc in range",
+     "def"
+]
 
 
 space = 0
@@ -31,7 +40,7 @@ with open('logo-out.py', 'w',) as out:
             space -= 4
             file = file[1:].lstrip()
             continue
-        if instruction.split(']')[0] not in allowed:
+        if instruction.split(']')[0] not in allowed_key:
             instruction = instruction.split(']')[0]
             out.write(f"{' ' * space}{instruction}()\n")
             file = file[len(instruction) :].lstrip()
@@ -41,18 +50,18 @@ with open('logo-out.py', 'w',) as out:
         if '[' in next_instruction:
             next_instruction = next_instruction.partition("[")[0]
         if instruction == 'repeat':
-            out.write(f"{' ' * space}{allowed[instruction.replace('[','')]}(1, {int(next_instruction)+1}):\n")
+            out.write(f"{' ' * space}{allowed_value[allowed_key.index(instruction.replace('[',''))]}(1, {int(next_instruction)+1}):\n")
             space += 4
         elif next_instruction.isnumeric() or "'" in next_instruction or '+' in next_instruction:
-            out.write(f"{' ' * space}t.{allowed[instruction.replace('[','')]}({next_instruction})\n")
+            out.write(f"{' ' * space}t.{allowed_value[allowed_key.index(instruction.replace('[',''))]}({next_instruction})\n")
         elif instruction == "to":
             space = 4
-            out.write(f"{allowed[instruction]} {next_instruction}():\n")
+            out.write(f"{allowed_value[allowed_key.index(instruction)]} {next_instruction}():\n")
         elif ']' in next_instruction:
-            out.write(f"{' ' * space}t.{allowed[instruction.replace('[','')]}({next_instruction.replace(']','')})\n")
+            out.write(f"{' ' * space}t.{allowed_value[allowed_key.index(instruction.replace('[',''))]}({next_instruction.replace(']','')})\n")
             space = 0
         else:
-            out.write(f"{' ' * space}t.{allowed[instruction]}()\n")
+            out.write(f"{' ' * space}t.{allowed_value[allowed_key.index(instruction)]}()\n")
             continue
         file = file[len(next_instruction) :].lstrip()
     out.write('turtle.done()\n')
