e8ae5807dd5ed32c0acbb27cf26118a5e97ad42e
Ivan Peťko <ipetko96@gmail.com>
2023-11-21 22:49:05 +0100

    logic that should work if completed

--- week8.py
+++ week8.py
@@ -3,33 +3,28 @@ class Stvorce:
         self.pole = [[0] * (n**2) for _ in range(n**2)]
 
     def urob(self, index):
+        self.slice_column = [len(self.pole) // 2, len(self.pole) // 2]
+        self.slice_row = [len(self.pole) // 2, len(self.pole) // 2]
         for i in index:
+            self.sub_pole = self.pole[:]
             if i == "1":
-                for i in range(
-                    len(
-                        [
-                            self.pole[i][0 : len(self.pole) // 2]
-                            for i in range(0, len(self.pole) // 2)
-                        ]
-                    )
-                ):
-                    for j in range(
-                        len(
-                            [
-                                self.pole[i][0 : len(self.pole) // 2]
-                                for i in range(0, len(self.pole) // 2)
-                            ][i]
-                        )
-                    ):
-                        if self.pole[i][j] == 0:
-                            self.pole[i][j] = 1
-                        else:
-                            self.pole[i][j] = 0
-                ...
+                self.sub_pole = [self.sub_pole[j][:len(self.sub_pole) // 2] for j in range(0, len(self.sub_pole) // 2)]
+                self.slice_row[0] -= len(self.sub_pole)
+                self.slice_column[1] += len(self.sub_pole)
             elif i == "2":
-                ...
+                self.sub_pole = [self.sub_pole[j][len(self.sub_pole) // 2:] for j in range(0, len(self.sub_pole) // 2)]
+                self.slice_column[0] -= len(self.sub_pole)
+                self.slice_row[1] += len(self.sub_pole)
             elif i == "3":
-                ...
+                self.pole = [self.pole[j][:len(self.pole) // 2] for j in range(len(self.pole) // 2, len(self.pole))]
+            elif i == "4":
+                self.pole = [self.pole[j][len(self.pole) // 2:] for j in range(len(self.pole) // 2, len(self.pole))]
+        for y in range(self.slice_column[0], self.slice_row[0]):
+            for x in range(self.slice_column[1], self.slice_row[1]):
+                if self.pole[y][x] == 0:
+                    self.pole[y][x] = 1
+                elif self.pole[y][x] == 1:
+                    self.pole[y][x] = 0
 
     def pocet(self):
         print(
@@ -45,7 +40,7 @@ stv = Stvorce(2)
 stv.pocet()
 stv.vypis()
 print()
-stv.urob("1")
+stv.urob("2")
 stv.pocet()
 stv.vypis()
 
