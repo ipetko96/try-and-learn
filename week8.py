9580f90b72a28611cac4124923a44ab6dceb3245
Ivan Peťko <ipetko96@gmail.com>
2023-11-22 23:12:11 +0100

    solved

--- week8.py
+++ week8.py
@@ -3,24 +3,28 @@ class Stvorce:
         self.pole = [[0] * (n**2) for _ in range(n**2)]
 
     def urob(self, index):
-        self.slice_column = [len(self.pole) // 2, len(self.pole) // 2]
-        self.slice_row = [len(self.pole) // 2, len(self.pole) // 2]
+        self.sub_pole = self.pole[:]
+        self.slice_start = [0, 0]
+        self.slice_end = [len(self.pole), len(self.pole)]
         for i in index:
-            self.sub_pole = self.pole[:]
             if i == "1":
                 self.sub_pole = [self.sub_pole[j][:len(self.sub_pole) // 2] for j in range(0, len(self.sub_pole) // 2)]
-                self.slice_row[0] -= len(self.sub_pole)
-                self.slice_column[1] += len(self.sub_pole)
+                self.slice_end[0] -= len(self.sub_pole)
+                self.slice_end[1] -= len(self.sub_pole)
             elif i == "2":
                 self.sub_pole = [self.sub_pole[j][len(self.sub_pole) // 2:] for j in range(0, len(self.sub_pole) // 2)]
-                self.slice_column[0] -= len(self.sub_pole)
-                self.slice_row[1] += len(self.sub_pole)
+                self.slice_start[1] += len(self.sub_pole)
+                self.slice_end[0] -= len(self.sub_pole)
             elif i == "3":
-                self.pole = [self.pole[j][:len(self.pole) // 2] for j in range(len(self.pole) // 2, len(self.pole))]
+                self.sub_pole = [self.sub_pole[j][:len(self.sub_pole) // 2] for j in range(len(self.sub_pole) // 2, len(self.sub_pole))]
+                self.slice_start[0] += len(self.sub_pole)
+                self.slice_end[1] -= len(self.sub_pole)
             elif i == "4":
-                self.pole = [self.pole[j][len(self.pole) // 2:] for j in range(len(self.pole) // 2, len(self.pole))]
-        for y in range(self.slice_column[0], self.slice_row[0]):
-            for x in range(self.slice_column[1], self.slice_row[1]):
+                self.sub_pole = [self.sub_pole[j][len(self.sub_pole) // 2:] for j in range(len(self.sub_pole) // 2, len(self.sub_pole))]
+                self.slice_start[0] += len(self.sub_pole)
+                self.slice_start[1] += len(self.sub_pole)
+        for y in range(self.slice_start[0], self.slice_end[0]):
+            for x in range(self.slice_start[1], self.slice_end[1]):
                 if self.pole[y][x] == 0:
                     self.pole[y][x] = 1
                 elif self.pole[y][x] == 1:
@@ -33,55 +37,22 @@ class Stvorce:
 
     def vypis(self):
         for i in self.pole:
-            print(i)
+            for j in i:
+                if j == 0:
+                    print('-', end='')
+                elif j == 1:
+                    print('X', end='')
+            print()
 
 
-stv = Stvorce(2)
+stv = Stvorce(4)
+vstup = ['1', '11', '131', '1314', '143', '12', '1233', '1234', '1243', '1244',
+         '23', '234', '2133', '2134', '2143', '2144', '24', '242', '2423',
+         '31', '313', '3132', '32', '324', '3232', '3411', '3412', '3421',
+         '3422', '4', '44', '424', '4241', '413', '4141', '43', '4311', '4312',
+         '4321', '4322']
+
+for i in vstup:
+    stv.urob(i)
 stv.pocet()
 stv.vypis()
-print()
-stv.urob("2")
-stv.pocet()
-stv.vypis()
-
-# 1 2
-# 3 4
-#   - - - - - - - - - - - - - - - -
-#   - - - - - - - - - - - - - - - -
-#   - - - - - - - - - - - - - - - -
-#   - - - - X X X X X X X X - - - -
-#   - - X X X X X X X X X X X X - -
-#   - X X X X X X X X X X X X X X -
-#   X X X X - - X X X X - - X X X X
-#   X X X X - - X X X X - - X X X X
-#   X X X X X X X X X X X X X X X X
-#   X X X X X X X X X X X X X X X X
-#   - X X X X - - - - - - X X X X -
-#   - - X X X X - - - - X X X X - -
-#   - - - - X X X X X X X X - - - -
-#   - - - - - - - - - - - - - - - -
-#   - - - - - - - - - - - - - - - -
-#   - - - - - - - - - - - - - - - -
-
-# 11 12
-# 13 14
-#   - - - - - - - -
-#   - - - - - - - -
-#   - - - - - - - -
-#   - - - - X X X X
-#   - - X X X X X X
-#   - X X X X X X X
-#   X X X X - - X X
-#   X X X X - - X X
-
-# 111 112
-# 113 114
-#   - - - -
-#   - - - -
-#   - - - -
-#   - - - -
-
-# 1111 1112
-# 1113 1114
-#   - -
-#   - -
