a95cdecd9c12f81ccd4aede3b1ecdedec557617d
Ivan Peťko <ipetko96@gmail.com>
2023-11-30 10:42:29 +0100

    solve robot and batoh methods

--- karel.py
+++ karel.py
@@ -12,23 +12,31 @@ class RobotKarel:
         with open(meno_suboru) as subor:
             lines = subor.readlines()
             firstLine = lines[0].strip().split()
-            self.pole = [["."] * (int(firstLine[1])) for _ in range(int(firstLine[0]))]
+            self.pole = [[["."] for _ in range(int(firstLine[1]))] for _ in range(int(firstLine[0]))]
             for line in lines[1:]:
                 line = line.strip().split()
-                self.pole[int(line[1])][int(line[2])] = line[0]
+                self.pole[int(line[1])][int(line[2])].append(line[0])
 
     def __str__(self):
-        return ""
+        return f"{self.pole}"
 
     def robot(self, riadok, stlpec, smer):
-        ...
+        self.karel = {
+            'y': riadok,
+            'x': stlpec,
+            'smer': smer,
+            'batoh': []
+        }
 
     def rob(self, prikaz):
         return 0
 
     def batoh(self):
-        return []
+        return self.karel['batoh']
 
 
-k = RobotKarel("subor1.txt")
-print(k)
+k = RobotKarel('subor1.txt')
+k.robot(0, 0, 0)
+# print(k)
+print(k.karel)
+print('batoh =', k.batoh())
