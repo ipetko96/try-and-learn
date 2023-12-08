cf6b6d46d305166db5498c6ac47e041483d10e51
Ivan Peťko <ipetko96@gmail.com>
2023-12-08 14:59:01 +0100

    expecting negative numbers, does not use eval, correctly returning methods

--- pajton.py
+++ pajton.py
@@ -15,35 +15,54 @@ class Pajton:
         retazec = retazec.split()
         for i, v in enumerate(retazec):
             if i % 2 == 0:
-                for j in v:
-                    if j in self.operacie:
-                        raise SyntaxError
-                if v.isnumeric():
-                    pass
-                else:
-                    try:
-                        v = self.prem(v)
-                    except NameError:
-                        raise SyntaxError
+                try:
+                    if int(v) < 0:
+                        pass
+                except ValueError:
+                    for j in v:
+                        if j in self.operacie:
+                            raise SyntaxError
+                    if v.isnumeric():
+                        pass
+                    else:
+                        try:
+                            v = self.prem(v)
+                        except NameError:
+                            raise NameError
                 if i != 0 and vysledok[-1] in self.operacie:
-                    vysledok = vysledok.replace("/", "//")
-                    vysledok = str(eval(vysledok + v))
+                    if vysledok[-1] == "+":
+                        vysledok = int(vysledok[:-1])
+                        vysledok += int(v)
+                        vysledok = str(vysledok)
+                    elif vysledok[-1] == "-":
+                        vysledok = int(vysledok[:-1])
+                        vysledok -= int(v)
+                        vysledok = str(vysledok)
+                    elif vysledok[-1] == "*":
+                        vysledok = int(vysledok[:-1])
+                        vysledok *= int(v)
+                        vysledok = str(vysledok)
+                    elif vysledok[-1] == "/":
+                        vysledok = int(vysledok[:-1])
+                        try:
+                            vysledok //= int(v)
+                            vysledok = str(vysledok)
+                        except ZeroDivisionError:
+                            vysledok = str(0)
                 else:
-                    vysledok += v
+                    vysledok += str(v)
             elif i % 2 == 1:
                 if v in self.operacie:
                     vysledok += v
                 else:
                     raise SyntaxError
-
         if vysledok[-1] in self.operacie:
             raise SyntaxError
-        return vysledok
+        return int(vysledok)
 
-    def prirad(self, retazec):
-        meno, hodnota = retazec.split(" = ")
+    def prirad(self, meno, hodnota):
         meno = meno.strip()
-        hodnota = hodnota.strip()
+        hodnota = str(hodnota)
         if meno == hodnota:
             raise SyntaxError
         for i in meno.strip():
@@ -55,7 +74,7 @@ class Pajton:
             raise SyntaxError
         if len(hodnota.split()) > 1:
             hodnota = self.vyraz(hodnota)
-        self.tab[meno] = hodnota
+        self.tab[meno] = int(hodnota)
         return None
 
     def prikaz(self, retazec):
@@ -64,10 +83,13 @@ class Pajton:
         if retazec == "dir()":
             return self.dir()
         if "prem(" in retazec:
-            return eval(f"self.prem('{retazec[5:-1]}')")
+            return self.prem(retazec[5:-1])
         if " = " in retazec:
-            return self.prirad(retazec)
-        print(self.vyraz(retazec))
+            meno, hodnota = retazec.split(" = ")
+            meno = meno.strip()
+            hodnota = hodnota.strip()
+            return self.prirad(meno, hodnota)
+        return self.vyraz(retazec)
 
     def dir(self):
         return self.tab.keys() | set()
@@ -77,15 +99,12 @@ class Pajton:
             return None
         data = ""
         for k, v in self.tab.items():
-            data += f"{k}:{v}\n"
+            data += f"{k}: {v}\n"
         return data[:-1]
 
 
 if __name__ == "__main__":
     p = Pajton()
-    # p.prikaz("a = 2")
-    # p.prikaz("b = a + 1")
-    # p.prikaz("c = 7 - b")
     while True:
         try:
             hodn = p.prikaz(input(">>> "))
