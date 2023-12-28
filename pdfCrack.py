86813eef4006005eaf1c6ba7a1671ae8f4650a81
Ivan Peťko <ipetko96@gmail.com>
2023-12-28 23:50:24 +0100

    cleanup

--- pdfCrack.py
+++ pdfCrack.py
@@ -1,25 +1,4 @@
-from pypdf import PdfWriter, PdfReader
-
-
-def add_encryption(input_pdf, output_pdf, password):
-    pdf_writer = PdfWriter()
-    pdf_reader = PdfReader(input_pdf)
-
-    for page in range(len(pdf_reader.pages)):
-        pdf_writer.add_page(pdf_reader.pages[page])
-
-    pdf_writer.encrypt(user_password=password, owner_pwd=None, use_128bit=True)
-
-    with open(output_pdf, "wb") as fh:
-        pdf_writer.write(fh)
-
-
-# if __name__ == "__main__":
-#     add_encryption(
-#         input_pdf="testpdf.pdf",
-#         output_pdf="testpdf_encrypted.pdf",
-#         password="9610231433",
-#     )
+from pypdf import PdfReader
 
 
 def crack_pdf(input_path, password):
@@ -28,11 +7,11 @@ def crack_pdf(input_path, password):
 
         for i in range(9999 + 1):
             suffix = str(i).rjust(4, "0")
+            print(password + suffix)
             if reader.decrypt(password + suffix):
                 print(f"password is: {password + suffix}")
                 break
 
 
 if __name__ == "__main__":
-    # example usage:
-    crack_pdf("encrypted.pdf", "700403")
+    crack_pdf("pdf.pdf", "")
