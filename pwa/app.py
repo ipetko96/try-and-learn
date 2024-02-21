476a1e609e2504386eda3330da7d45a91c77f0b6
Ivan Peťko <ipetko96@gmail.com>
2024-02-21 16:17:52 +0100

    correctly cache website

--- pwa/app.py
+++ pwa/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/manifest.json")
+def serve_manifest():
+    return send_file("manifest.json", mimetype="application/manifest+json")
+
+
+@app.route('/service-worker.js')
+def serve_sw():
+    return send_file('service-worker.js', mimetype='application/javascript')
